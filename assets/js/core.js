7481f2cbc159dfbadd5aecea5ac8606637f8b923
funnyjk <funnyjk@gmail.com>
2014-05-01 16:22:47 -0400

    adding core.js

---
+++ assets/js/core.js
@@ -0,0 +1,16 @@
+//assets/core.js
+var telescopejs = angular.module('telescopejs', []);
+
+function mainController($scope, $http) {
+	$scope.formData{};
+	
+	//When landing on page, get all connected telescopes and show them
+	$http.get('/api/telescope')
+		.success(function(data) {
+			$scope.telescopes = data;
+			console.log(data);
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+}
