ed86bc4bb7196401c0ab65836678db4c9c26a55d
funnyjk <funnyjk@gmail.com>
2014-05-01 16:15:23 -0400

    angularjs

--- server.js
+++ server.js
@@ -3,18 +3,45 @@ var
   app = express(),
   server = require('http').createServer(app),
   io = require('socket.io').listen(5000),
-  fs = require('fs');
+  fs = require('fs'),
+  mongoose = require('mongoose');
+  
+//Mongoose Configure	=======================
+mongoose.connect('mongodb://localhost/telescope');
 
-//Express hosting the file folders 
+//Express Configure		=======================
 app.configure(function(){
   app.use(express.static(__dirname + '/public'));
   app.use(express.static(__dirname + '/assets'));
+  app.use(express.bodyParser());
 });
   
+//Define Model
+var client = mongoose.model('client', {
+	socketid: String
+});	
+var telescope = mongoose.model('telescope', {
+	socketid: String
+});
+
+//Routes 				=======================
 app.get('/', function(req, res){
   res.sendfile(__dirname + 'public/index.html');
 })
-  
+
+//=======================API==============================================
+	//List All Telescopes
+	app.get('/api/telescope', function(req, res) {
+		//Mongoose
+		telescope.find(function(err, telescope) {
+			//error checking
+			if (err)
+				res.send(err)
+			res.json(telescope);
+		});
+	});
+
+//Express Port
 var port = 80;
 app.listen(port, function() {
 	console.log("Listening on " + port);
