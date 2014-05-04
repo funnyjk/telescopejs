ad3a14c37573b62375e2436865adc93f33a19a7d
funnyjk <funnyjk@funnyjk-1000HE.(none)>
2014-05-04 19:14:56 -0400

    Various Telescope Controls

--- telescope.js
+++ telescope.js
@@ -53,7 +53,7 @@ function hex2a(hexx) {
 	var hexa = hexx.split(' ').join('');
 	var hex = hexa.toString();
 	var str = '';
-	for (var i = 0, i < hex.length; i += 2)
+	for (var i = 0; i < hex.length; i += 2)
 		str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
 	return str;
 }
