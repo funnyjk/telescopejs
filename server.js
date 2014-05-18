d4f66dded8560b1df3cf8f0f353235e0bf57fdd4
funnyjk <funnyjk@funnyjk-1000HE.(none)>
2014-05-17 23:00:17 -0400

    video streaming

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ io.sockets.on('connection', function(socket) {
 		streamHeader.write(STREAM_MAGIC_BYTES);
 		streamHeader.writeUInt16BE(width, 4);
 		streamHeader.writeUInt16BE(hight, 6);
-		socket.send(streamHeader, {binary:true});
+		socket.emit('data', streamHeader, {binary:true});
 		
 		//Add to Mongoose
 		var client = new clientModel();
